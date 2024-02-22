160780556c26fa70ec0e96f79111cdfa2a71db62
Lawrence Hwang <lawrenceh326@gmail.com>
2024-02-22 15:40:21 -0800

    Progress save for checking color

--- vision/src/rgbd_segmentation.py
+++ vision/src/rgbd_segmentation.py
@@ -12,6 +12,9 @@ class RGBDSegmentation(object):
         self.rgb_img_sub = rospy.Subscriber(f"/{cam_spec}/camera/color/image_raw_throttled", Image, self.rgb_callback, (cam_spec), queue_size=1)
         self.depth_img_camera_info = rospy.Subscriber(f"/{cam_spec}/camera/aligned_depth_to_color/camera_info_throttled",CameraInfo, self.depth_cam_info_callback,queue_size=1)
 
+        self.plant_rgb_img_sub = rospy.Subscriber(f"/{cam_spec}/camera/color/image_raw", Image, self.plant_rgb_callback, (cam_spec), queue_size=1)
+
+
         # Publishers with segmented image info
         self.image_pub = rospy.Publisher(f"/{cam_spec}/rs_segmented_image", Image, queue_size=1)
         self.depth_image_pub = rospy.Publisher(f"/{cam_spec}/seg_depth/image_raw", Image, queue_size=1)
@@ -22,6 +25,72 @@ class RGBDSegmentation(object):
         self.depth_data = []
         self.depth_cam_info = CameraInfo()
         self.seg_depth_img = Image()
+
+    def plant_rgb_callback(self, data, args):
+        try:
+            cv_image = self.bridge_object.imgmsg_to_cv2(data, desired_encoding="bgr8")
+        except CvBridgeError as e:
+            print(e)
+        rospy.sleep(0.01)
+
+        view_analysis_count = { # dictionary
+            "top" : 0,
+            "bottom" : 0,
+            "left": 0,
+            "right": 0,
+        }
+
+        # print(data.height, data.width) # 720 1280
+        for v in range(data.width): #1280px
+            if v == 0 or v == 1279:
+                # check the whole height, 
+                # meaning we for loop down w
+                pass
+                # for w in data.height:
+                #     b, g, r = cv_image[y, x]
+            else:
+                # we only check w's 0 or 719 at the current v
+
+                # top 
+                b, g, r = cv_image[0, v] # [y, x]
+                # check this bgr
+                top_color_check_bool = self.check_color(b,g,r)
+                # if the check is green or purple, break
+                if top_color_check_bool == True:
+                    view_analysis_count["top"] = view_analysis_count["top"] + 1 # increase counter for top
+
+                # bottom
+                b, g, r = cv_image[719, v]
+                # check this bgr
+                bottom_color_check_bool = self.check_color(b,g,r)
+                # if the check is green or purple, break
+                if bottom_color_check_bool == True:
+                    view_analysis_count["bottom"] = view_analysis_count["bottom"] + 1 # increase counter for top
+
+        threshold_value = 25
+        threshold_check = any(value > threshold_value for value in view_analysis_count.values())
+        if threshold_check == True:
+            # let us know, determine the direction for the arm to move
+            print(threshold_check)
+            print(view_analysis_count)
+
+    def check_color(self, b,g,r):
+        # we receive the bgr from the image, 
+        # now check if that bgr value is within a
+        # range of green or purple we set
+
+        # Define the lower and upper bounds of the green color range in BGR format
+        lower_green = np.array([0, 50, 0], dtype=np.uint8)
+        upper_green = np.array([50, 255, 50], dtype=np.uint8)
+        
+        # our bgr value
+        bgr_value = np.array([b, g, r], dtype=np.uint8)
+
+        # Create a mask to check if the BGR value is within the green range
+        mask = cv2.inRange(bgr_value, lower_green, upper_green)
+        return mask[1][0] == 255 # if mask value is 255, BGR value is within the green range
+            
+
 
     def rgb_callback(self, data, args):
         try:
@@ -55,6 +124,10 @@ class RGBDSegmentation(object):
 
 
         new_img = cv2.bitwise_and(cv_image, cv_image, mask = combined_mask )
+        # # Display the resulting frame
+        # resized_frame = cv2.resize(new_img, (0,0), fx=0.80, fy=0.80)
+        # cv2.imshow('new_img', resized_frame) 
+        # cv2.waitKey(1)
 
         # dilation
         kernel = np.ones((5,5), np.uint8)
@@ -104,18 +177,17 @@ class RGBDSegmentation(object):
         # Segmented RGB Image
         segmented_img = self.bridge_object.cv2_to_imgmsg(new_img,"passthrough")
         segmented_img.header.frame_id = f"{optical_frame_prefix}_color_optical_frame"
+        # print(new_img)
 
         # Segmented Depth Image
         self.seg_depth_img = self.bridge_object.cv2_to_imgmsg(new_depth_img)
         self.seg_depth_img.header.stamp = cam_info.header.stamp
         self.seg_depth_img.header.frame_id = f"{optical_frame_prefix}_color_optical_frame"
 
-        # Display the resulting frame
+        # # Display the resulting frame
         # resized_frame = cv2.resize(new_img, (0,0), fx=0.80, fy=0.80)
         # cv2.imshow('Preview', segmented_img) 
         # cv2.waitKey(1)
-
-        # print("new:\n",self.seg_depth_img)
     
         # Publish
         self.image_pub.publish(segmented_img)
